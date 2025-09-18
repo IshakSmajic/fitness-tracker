9f99d5641cdb29e34f49f330affa5acc1fb73018
Ishak <ishaksmajicpac@gmail.com>
2025-09-18 23:06:06 +0200

    Skeleton for classes

--- fitness-tracker.py
+++ fitness-tracker.py
@@ -5,4 +5,19 @@ def main():
     print("3. Exit")
 
 
+def add_workout():
+    print("Placement function for adding a workout")
+
+
+def view_workout_history():
+    print("Placement function for viewing workout history")
+
+
 main()
+choice = input("Choose option: ")
+if choice == "1":
+    add_workout()
+elif choice == "2":
+    view_workout_history()
+elif choice == "3":
+    print("Exiting the app")
