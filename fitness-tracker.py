7ff75770841c9b7b775efa2fee065d5330625020
Ishak <ishaksmajicpac@gmail.com>
2025-09-21 18:44:22 +0200

    Added meals

--- fitness-tracker.py
+++ fitness-tracker.py
@@ -1,11 +1,13 @@
 workouts = []
+meals = []
 
 
 def main():
     print("This is a fitness tracker app(in development)")
     print("1. Add workout")
     print("2. View workout history")
-    print("3. Exit")
+    print("3. Add meal")
+    print("4. Exit")
 
 
 def add_workout():
@@ -19,6 +21,14 @@ def view_workout_history():
         print(f"{i}. {workout}. ")
 
 
+def add_meal():
+    meal_name = input("Enter meal name: ")
+    calories = input("Enter calories: ")
+    meal = {"name": meal_name, "calories": calories}
+    meals.append(meal)
+    print("Meal added!")
+
+
 main()
 choice = input("Choose option: ")
 if choice == "1":
@@ -26,4 +36,6 @@ if choice == "1":
 elif choice == "2":
     view_workout_history()
 elif choice == "3":
+    add_meal()
+elif choice == "4":
     print("Exiting the app. ")
